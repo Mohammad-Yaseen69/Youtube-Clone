902f468149589cc672f2f67268ca33f6554a70a3
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-29 16:40:25 +0500

    Defining All the components file and adding routes

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import ChannelDetail from "./ChannelDetail";
+import Feed from "./Feed";
+import Navbar from "./Navbar";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+
+
+export {Navbar , SearchFeed ,Feed ,VideoDetail ,ChannelDetail}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider ,Routes , Route , createRoutesFromElements , createBrowserRouter} from 'react-router-dom'
+import {Feed ,ChannelDetail,SearchFeed,VideoDetail} from './components'
 
 
 const router = createRoutesFromElements(
   <Route path='/' element={<App />}>
-
+    <Route path='/' element={<Feed />} />
+    <Route path='video/:id' element={<VideoDetail />} />
+    <Route path='channel/:id' element={<ChannelDetail />} />
+    <Route path='search/:searchTerm' element={<SearchFeed />} />
   </Route>
 ) 
 
