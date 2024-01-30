2e78d47118988447e1153f8bfe898f4faa6dd11c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-30 20:49:29 +0500

    Building Channel Detail component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -2,6 +2,8 @@ import { CheckCircle } from '@mui/icons-material'
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../assets'
+
 
 const ChannelCard = ({ channelDetail }) => {
   return (
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ChannelCard, VideoCard ,Videos} from './'
+import { fetchData } from '../logic/Fetching'
 
 const ChannelDetail = () => {
+  const ChannelId = useParams().id
+  const [channel, setChannel] = useState()
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchData(`channels?part=snippet&id=${ChannelId}`).
+      then(data => {
+        setChannel(data.items[0])
+      })
+
+    fetchData(`search?part=snippet&channelId=${ChannelId}&order=date`)
+      .then(data => {
+        setVideos(data.items)
+      })
+  }, [ChannelId])
+
+ 
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh" justifyContent={'center'}>
+    <Box>
+      <div style={{
+        height:'200px',
+        background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+        zIndex: 10,
+      }} />
+      <ChannelCard channelDetail={channel} marginTop="-93px" />
+    </Box>
+    <Box p={2} display="flex">
+    <Box sx={{ mr: { sm: '100px' } }}/>
+      <Videos Videos={videos} />
+    </Box>
+  </Box>
   )
 }
 
